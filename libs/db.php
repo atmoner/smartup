780b94b64353e7b67218f6220077500a26f99455
atmoner <contact.atmoner@gmail.com>
2017-03-02 21:01:34 +0100

    First commit

--- libs/db.php
+++ libs/db.php
@@ -2,6 +2,6 @@
 // Generate For Bittytorrent
 $dbhost = "localhost";
 $dbuser = "root";
-$dbpass = "toor";
-$dbname = "opendata";
+$dbpass = "****";
+$dbname = "*******";
 // Please ! manipulate this file if you know what you made​​!
